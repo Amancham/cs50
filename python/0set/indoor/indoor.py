a98c26989a6791ed35c996bb32b4c96ea9e727a4
Amancham <amancham@gmx.net>
2024-10-20 14:15:52 +0200

    indoor voices first try

---
+++ python/0set/indoor/indoor.py
@@ -0,0 +1,13 @@
+# solution for problem set 0: Indoor Voice 
+
+def main():
+	# store user input in a str variable 
+	yell = input("")
+	# change str to all lowercase spelling
+	whisper = str.lower(yell)
+	# output changed string to user
+	print (whisper) 
+
+# set up execution of function 
+if __name__ == "__main__": 
+	main()
