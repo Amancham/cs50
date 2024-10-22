106c12c82618d6b5da47726b61d7672ac863ad10
Amancham <amancham@gmx.net>
2024-10-22 13:29:23 +0200

    solution to playback Speed added

---
+++ python/0set/playback/playback.py
@@ -0,0 +1,10 @@
+# solution for Playback speed 
+
+# grab userinput and replace whitespace with "..." then print changed string
+def main():
+	user_input = input("").replace(" ", "...")
+	print(user_input) 
+
+# only run when we want it to run 
+if __name__ == "__main__":
+	main()
